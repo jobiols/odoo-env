4b477db4d66068e2622021adf683b1145fa29bd0
jobiols <jorge.obiols@gmail.com>
2018-04-01 12:48:21 -0300

    [DOC]  -  readme

--- data/install_scripts.sh
+++ data/install_scripts.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
 # install sd and oe
+echo
+echo "installing oe"
 echo "~/odoo-env/scripts/oe.py \$*" > /usr/local/bin/oe
 chmod +x /usr/local/bin/oe
-cp ../scripts/sd.py /usr/local/bin/sd
+echo
+echo "installing sd"
+cp odoo-env/scripts/sd.py /usr/local/bin/sd
 
 # install python 2.7
+echo
+echo "installing python"
 apt-get install python
 
 # install docker
+echo
+echo "installing docker"
 git clone https://github.com/docker/docker-install.git ~/tmp
 bash ~/tmp/install.sh
+rm -r /tmp

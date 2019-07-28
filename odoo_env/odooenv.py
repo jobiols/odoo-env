a9f69578dcd3220f4613c8de7a57f9296fabde53
jobiols <jorge.obiols@gmail.com>
2019-07-28 03:00:35 -0300

    [FIX] - CreateGitignore falla en python > 2.7

--- odoo_env/odooenv.py
+++ odoo_env/odooenv.py
@@ -3,8 +3,8 @@
 try:
     from client import Client
     from command import Command, MakedirCommand, ExtractSourcesCommand, \
-        CloneRepo, PullRepo, CreateNginxTemplate, MessageOnly, PullImage, \
-        CreateGitignore
+        CloneRepo, CreateNginxTemplate, MessageOnly, CreateGitignore, \
+        PullImage, PullRepo
     from constants import BASE_DIR, IN_CONFIG, IN_DATA, IN_LOG, \
         IN_CUSTOM_ADDONS, IN_DIST_PACKAGES, IN_EXTRA_ADDONS, IN_BACKUP_DIR
 except ImportError:

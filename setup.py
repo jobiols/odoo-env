7b528086ccf0621e0d4e2b5003ab7e7cdc2bb753
jobiols <jorge.obiols@gmail.com>
2019-09-29 12:59:44 -0300

    [FIX] agregar dependencia six que falta cuando se instala en Gentoo

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
             'sd=odoo_env.sd:main'
         ],
     },
-    install_requires=['PyYAML'],
+    install_requires=['PyYAML', 'six'],
     data_files=[('', ['odoo_env/data/nginx.conf'])],
     packages=setuptools.find_packages(),
     classifiers=[

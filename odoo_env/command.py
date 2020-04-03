ebe4b2b2041af3c1ed2cdbc04de1b5d668b494fa
jobiols <jorge.obiols@gmail.com>
2020-04-02 22:26:53 -0300

    [FIX] ubicacion de nginx.conf

--- odoo_env/command.py
+++ odoo_env/command.py
@@ -126,8 +126,7 @@ class CreateNginxTemplate(Command):
 
     def execute(self):
         # leer el nginx.conf
-        path = os.path.dirname(os.path.abspath(__file__))
-        with open(path + '/nginx.conf', 'r') as f:
+        with open('/usr/local/nginx.conf', 'r') as f:
             conf = f.read()
 
         # poner el nombre del cliente en el config

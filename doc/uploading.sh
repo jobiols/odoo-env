bcb95191ba8280315b84b5c7a90765e6f00d85ec
jobiols <jorge.obiols@gmail.com>
2020-04-05 15:10:45 -0300

    fixup! [FIX] 0.10.0 New manifest syntax, backward compatible.

---
+++ doc/uploading.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Generating distribution archives
+# ################################
+
+# verify last version
+python -m pip install --user --upgrade setuptools wheel twine
+ 
+# ejecutar en el directorio donde esta setup.py
+    
+python setup.py sdist bdist_wheel
+    
+# Uploading the distribution archives
+    
+twine upload dist/*
+    
+# luego el proyecto se puede ver en
+# https://pypi.org/project/odoo-env/
+
+# Installing your newly uploaded package
+# pip install odoo-env

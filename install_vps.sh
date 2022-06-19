bd450a752d179f639d28392be6a7da587d5b4307
Jorge Obiols <jorge.obiols@gmail.com>
2022-06-19 20:20:38 -0300

    [FIX] bump docker-compose version

--- install_vps.sh
+++ install_vps.sh
@@ -38,7 +38,7 @@ rm get-docker.sh
 
 # install composer
 # Run this command to download the current stable release of Docker Compose:
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 #  Apply executable permissions to the binary:
 sudo chmod +x /usr/local/bin/docker-compose

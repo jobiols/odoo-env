4ab7f269ec211bc350b6cbf3d840eb7742896b74
Jorge Obiols <jorge.obiols@gmail.com>
2023-10-05 19:43:20 -0300

    [FIX] 0.11.1

--- doc/uploading.sh
+++ doc/uploading.sh
@@ -3,18 +3,7 @@
 # Generating distribution archives
 # ################################
 
-# verify last version
-sudo python3 -m pip install --user --upgrade setuptools wheel twine
+pip install --upgrade build
 
 # ejecutar en el directorio donde esta setup.py
-sudo python3 setup.py sdist bdist_wheel
-
-# Uploading the distribution archives
-
-twine upload dist/*
-
-# luego el proyecto se puede ver en
-# https://pypi.org/project/odoo-env/
-
-# Installing your newly uploaded package
-# pip install odoo-env
+sudo python3 -m build

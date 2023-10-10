7ed4cb441b94a2075bcc2bb56acaf04983836f99
Jorge Obiols <jorge.obiols@gmail.com>
2023-10-10 14:04:07 -0300

    [UPD] improve docker installation

--- install_vps.sh
+++ install_vps.sh
@@ -17,27 +17,51 @@ python3 -V
 sudo apt install python3-distutils -y
 
 # install pip
-curl -fsSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python3 get-pip.py
-rm get-pip.py
+#curl -fsSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#sudo python3 get-pip.py
+#rm get-pip.py
 
 # install pipx
-#sudo apt install pipx
+sudo apt install pipx
+pipx --version
 
 # test pip
 #pip -V
 
 # install sd and oe
-echo
-echo "installing oe"
-pip install odoo-env
+pipx install ensurepath
+pipx install odoo-env
 
-# install docker
+# install docker en desarrollo
 echo
 echo "installing docker"
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 rm get-docker.sh
+
+# install docker en produccion Ubuntu
+# https://docs.docker.com/engine/install/ubuntu/
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# install latest versin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verificar docker
+sudo docker run hello-world
+
 
 # install composer
 # Run this command to download the current stable release of Docker Compose:

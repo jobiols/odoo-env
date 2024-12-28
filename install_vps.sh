bdba59e2f4a90ab7d9074d6467347c0d73b16adc
Jorge Obiols <jorge.obiols@gmail.com>
2024-12-28 19:19:47 -0300

    actualizar manual

--- install_vps.sh
+++ install_vps.sh
@@ -56,19 +56,20 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
+# Add the repository to Apt sources: EN UBUNTU
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
+
 # install latest versin
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Verificar docker
 sudo docker run hello-world
-
-
-# install composer
-# Run this command to download the current stable release of Docker Compose:
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-#  Apply executable permissions to the binary:
-sudo chmod +x /usr/local/bin/docker-compose
 
 # test
 docker-compose --version
